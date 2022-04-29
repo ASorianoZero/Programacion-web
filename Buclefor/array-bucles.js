0a211001c321c321f229e937d877455701a828f6
ASorianoZero <104519350+ASorianoZero@users.noreply.github.com>
2022-04-29 13:41:48 -0500

    Archivos ciclo For
    Simón

---
+++ Buclefor/array-bucles.js
@@ -0,0 +1,34 @@
+// Declaramos un array
+
+var pokemones = ["Charizard", "Venosaur", "Blastoise"];
+
+//Definir nuestra variable i
+
+//var i = 0;
+
+//Ciclo While
+
+/*
+while(i <= 2){
+    //Código que se ejecuta
+    console.log(pokemones[i]);
+    i++;    
+}
+*/
+
+//Ciclo Do/While
+
+/*
+do{
+    //Código a ejecutar
+    console.log(pokemones[i]);
+    i++;
+} while(i <= 2);
+*/
+
+//Ciclo for
+
+for(var i = 0; i <= 2; i++){
+    //Código que se ejecuta
+    document.write(pokemones[i] + " Yo te elijo.<br>");
+}
---
+++ Buclefor/code.js
@@ -0,0 +1,26 @@
+/*
+
+determinado
+
+Su sintaxsis consta de 3 partes:
+    -Variable
+    -Número de Iteraciones
+    -Incrementación o Decrementación
+Separadas por ";"
+
+for(variable; numero-iteraciones; incrementación/decrementación){
+    Código que se ejecuta
+}
+*/
+
+//Ciclo For incremental
+
+/*
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+*/
+
+for (var i = 10; i > 0; i --){
+    console.log(i);
+}
