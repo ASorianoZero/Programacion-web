63c8b50f39fa220e7162c4bf6570c1790b383fbf
ASorianoZero <104519350+ASorianoZero@users.noreply.github.com>
2022-06-03 12:16:14 -0500

    Arrays anidados 3

--- Arrays anidados/Code.js
+++ Arrays anidados/Code.js
@@ -21,24 +21,47 @@ mostrar.addEventListener('click', mostrarPokemones);
 
 
 function agregarpokemon(){
+    /*Condicional if*/
+if(nombre.value, tipo.value, NoPokedex.value === '') {
+    //Código que se ejecutará
+    mensaje.innerHTML = 'No se quiera pasar de listo!!'
+} else {
+    //Ejecuta esto si no se cumple la condición
     pokemones.push([nombre.value, tipo.value, NoPokedex.value]);
 
     //Imprimimos el mensaje en la etiqueta p id mensaje
 
     var i =pokemones.length - 1;
+    
+    
+    for(var r = 3; r > 0 ; r--){
+            mensaje.innerHTML = 'No hay nada mijo!!';
+    }
 
     mensaje.innerHTML = 'Se agregó ' + pokemones[i][0];
+};
+  
 }
 
 function mostrarPokemones(){
     //Código a ejecutar
+    //Condición If/Else
+    if(pokemones.length == 0){
+        mensaje.innerHTML = 'No hay nada mijo!!';
+    }
+    /*Ciclo for -> Repetir Código*/
 
+for(var r = 0; r < 3 ; r++){
+    /*Código que se repite*/
     document.write(
-    '<ul>' +
-        '<li><img src="' +  pokemones[0][0] + '.WEBP" alt="Imagen Pokimon"></li>' +	
-        '<li>Pokemon: ' + pokemones[0][0] + '</li>' + 
-        '<li>Tipo: ' + pokemones[0][1] + '</li>' +
-        '<li>No. Pokedex: ' + pokemones[0][2] + '</li>' +
-    '</ul>'
-    ); 
+        '<ul>' +
+            '<li><img src="' +  pokemones[r][0] + '.png" alt="Imagen Pokimon"></li>' +	
+            '<li>Pokemon: ' + pokemones[r][0] + '</li>' + 
+            '<li>Tipo: ' + pokemones[r][1] + '</li>' +
+            '<li>No. Pokedex: ' + pokemones[r][2] + '</li>' +
+        '</ul>'
+        ); 
+    };     
 }
+
+
