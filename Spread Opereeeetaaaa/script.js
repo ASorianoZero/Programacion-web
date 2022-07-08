9ec6fd5cabb5a4ea052794327cdeb326e94b43c0
ASorianoZero <104519350+ASorianoZero@users.noreply.github.com>
2022-07-08 13:05:58 -0500

    Spread Operator

---
+++ Spread Opereeeetaaaa/script.js
@@ -0,0 +1,25 @@
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(...numbers);
+
+// const addNumbers = (a,b,c) => {
+//     console.log(a + b + c);
+// }
+
+// let numbers = [1,2,3];
+
+// addNumbers(...numbers);
+
+
+// let pokemones = ['Mewtwo', 'Gardevoir', 'Rayquaza']
+
+// let newPokemones = ['Ditto', 'Evee'];
+
+// pokemones.push(newPokemones);
+
+// console.log(pokemones);
+
+// let numbers1 = [1,2,3,4,5]
+// let copyNumbers = numbers;
+
+// console.log(copyNumbers)
